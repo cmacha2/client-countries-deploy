495e2b210fd5bffaa9b60c4aae735ab9e5243aa5
cmacha2 <cristiancmg127@gmail.com>
2022-06-18 16:36:22 -0400

    actualizando url

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -1,5 +1,6 @@
 import { ADD_CHATBOT_INFO, CURRENT_PAGE, GET_ALL_ACTIVITIES, GET_COUNTRIES, GET_COUNTRIES_DETAILS, GET_COUNTRIES_FOR_CONTINENT, GET_COUNTRIES_MATCH, GET_COUNTRIES_SORT, GET_FILTER_ACTIVITIES, HANDLER_PAGINATION, POST_ACTIVITY, SHOW_ALL_ACTIVITIES, SWITCH_THEME } from "../actions-types";
 import axios from "axios"
+const URL_BACK = 'https://travelworld-api.herokuapp.com/'
 
 export function addChatBotInfo(payload) {
   return { type: ADD_CHATBOT_INFO, payload };
@@ -7,7 +8,7 @@ export function addChatBotInfo(payload) {
 
 export function getCountries(){
   return async function (dispatch){
-    const {data,status} = await axios.get("http://localhost:3001/countries")
+    const {data,status} = await axios.get(`${URL_BACK}/countries`)
     dispatch({type: GET_COUNTRIES, payload:[data,status] })
   }
 }
@@ -15,7 +16,7 @@ export function getCountries(){
 export function getCountriesMatch(name){
   return async function (dispatch){
     try {
-      const {data ,status} = await axios.get(`http://localhost:3001/countries?name=${name}`)
+      const {data ,status} = await axios.get(`${URL_BACK}/countries?name=${name}`)
       dispatch({type: GET_COUNTRIES_MATCH, payload:[data,status] })
     } catch (error) {
       return dispatch({type: GET_COUNTRIES_MATCH, payload:404 })
@@ -26,21 +27,21 @@ export function getCountriesMatch(name){
 
 export function getCountriesDetails(id){
   return async function (dispatch){
-    const {data} = await axios.get(`http://localhost:3001/countries/${id}`)
+    const {data} = await axios.get(`${URL_BACK}/countries/${id}`)
     dispatch({type: GET_COUNTRIES_DETAILS, payload:data })
   }
 }
 
 export function showAllActivies(){
   return async function (dispatch){
-    const {data} = await axios.get("http://localhost:3001/allActivities")
+    const {data} = await axios.get(`${URL_BACK}/allActivities`)
     dispatch({type: SHOW_ALL_ACTIVITIES, payload:data})
   }
 }
 
 export function getAllActivies(){
   return async function (dispatch){
-    const {data} = await axios.get("http://localhost:3001/countries")
+    const {data} = await axios.get(`${URL_BACK}/countries`)
     dispatch({type: GET_ALL_ACTIVITIES, payload:data})
   }
 }
@@ -58,7 +59,7 @@ export function setCurrentPage(currentPage){
 
 export function postActivity(body){
   return async function (dispatch){
-    const {data} = await axios.post(`http://localhost:3001/activities`,body)
+    const {data} = await axios.post(`${URL_BACK}/activities`,body)
     
     dispatch({type: POST_ACTIVITY, payload:data })
   }
@@ -66,7 +67,7 @@ export function postActivity(body){
 
 export function filterForContinent(searchContinent){
   return async function (dispatch){
-    const {data} = await axios.get("http://localhost:3001/countries")
+    const {data} = await axios.get(`${URL_BACK}/countries`)
     dispatch({type: GET_COUNTRIES_FOR_CONTINENT, payload:[ searchContinent,...data] })
   }
 }
