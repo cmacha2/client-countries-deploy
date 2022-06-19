f49380b75d344868636473d4afb681e54ce258f2
cmacha2 <cristiancmg127@gmail.com>
2022-06-19 19:21:48 -0400

    actualizacion del local storage

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { switchTheme } from '../../Redux/actions';
 import { ContainerSwitch, InputCheckBox, LaberlSwitch, LaberTheme, SpanSlider } from './Switch.css';
