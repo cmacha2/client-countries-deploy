cbf901c9cf40f3077b1b0bec44cc691b4e72f692
cmacha2 <cristiancmg127@gmail.com>
2022-06-21 01:51:41 -0400

    actualizacion del extencion de redux

--- src/Redux/store/index.js
+++ src/Redux/store/index.js
@@ -4,8 +4,10 @@ import thunk from "redux-thunk";
 
 const store = createStore(
     rootReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()),
+    compose(applyMiddleware(thunk), typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+    ? a => a
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()),
   );
 
 
