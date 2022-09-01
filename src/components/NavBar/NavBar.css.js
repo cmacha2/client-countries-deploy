d74b766046dcd44f431d138bf7d079e99c316be9
Cristian Machado <61942599+cmacha2@users.noreply.github.com>
2022-09-01 15:08:17 -0700

    Update NavBar.css.js

--- src/components/NavBar/NavBar.css.js
+++ src/components/NavBar/NavBar.css.js
@@ -2,20 +2,22 @@ import styled from "styled-components"
 
 
 export const Wrapper = styled.div`
-    display: flex;
     height: 5.2rem;
+    background-color: red;
     background-color: ${({theme})=> theme.bgc};
-    padding: 1.7rem;
+    /* padding: 1.7rem; */
     -webkit-box-shadow: 0px 0px 2px -1px rgba(20,19,20,1);
     -moz-box-shadow: 0px 0px 2px -1px rgba(20,19,20,1);
     box-shadow: 0px 0px 8px -1px rgba(10,9,20,1);
 `
 
 export const ContainerIMG = styled.div`
-    position: absolute;
-    top: 0;
-    padding-top: 0.9rem;
+    display:flex;
+    justify-content:flex-start;
+  padding: 1rem 1.3rem;
     @media (max-width: 600px) {
-    display:none;
+        display:flex;
+    align-items: center;
+    justify-content:center;
   }
 `
