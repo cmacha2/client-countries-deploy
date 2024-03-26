fa5441996de395fb703fa3a2f42bc5c7798fead4
Cristian Machado <61942599+cmacha2@users.noreply.github.com>
2024-03-26 12:25:08 -0400

    Update FilterByActivities.jsx
    Fixing the loop infinite

--- src/components/OptionSelections/FilterByActivities.jsx
+++ src/components/OptionSelections/FilterByActivities.jsx
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllActivies, getCountries, getCountriesSort, getFilterActivities, showAllActivies } from "../../Redux/actions";
+import {
+  getAllActivies,
+  getCountries,
+  getFilterActivities,
+  showAllActivies,
+} from "../../Redux/actions";
 import { ContainerMenu, Options, Selectors } from "./OptionSelect.css";
 
 export default function FilterByActivities() {
   const [nameActivity, setNameActivity] = useState('');
-  const dispatch = useDispatch(); 
-  const allActivities = useSelector(state => state.allActivities);
-  if (!allActivities.length)dispatch(showAllActivies());
+  const dispatch = useDispatch();
+  const allActivities = useSelector((state) => state.allActivities);
+
+  // useEffect para cargar todas las actividades al montar el componente
+  useEffect(() => {
+    if (!allActivities.length) {
+      dispatch(showAllActivies());
+    }
+  }, [allActivities.length, dispatch]); // Añadido dispatch a las dependencias por precaución
 
   const onChange = (e) => {
     setNameActivity(e.target.value);
   };
 
+  // useEffect que depende de nameActivity
   useEffect(() => {
-     dispatch(showAllActivies())
-  }, []);
+    if (nameActivity === 'allCountries') dispatch(getCountries());
+    if (nameActivity === 'allActivities') dispatch(getAllActivies());
+    if (nameActivity !== 'allCountries' && nameActivity !== '') dispatch(getFilterActivities(nameActivity));
+  }, [nameActivity, dispatch]); // Añadido dispatch a las dependencias
 
-  useEffect(() => {
-    if(nameActivity==='allCountries')dispatch(getCountries()) 
-    if(nameActivity==='allActivities')dispatch(getAllActivies()) 
-    if(nameActivity!=='allCountries'&&nameActivity!=='') dispatch(getFilterActivities(nameActivity)) 
-  }, [nameActivity]);
-
-
-  
   return (
-     <ContainerMenu>
-     <Selectors  name="activities" id="activities" onChange={onChange}>
-        <Options value={'allCountries'} >Filter by Activities</Options>
-        <Options value={'allActivities'}>All containt Activities</Options>
-        {allActivities.length && allActivities?.map((act)=><Options key={act.id} value={act.name}>{act.name}</Options>)}
-    </Selectors>
+    <ContainerMenu>
+      <Selectors name="activities" id="activities" onChange={onChange}>
+        <Options value={'allCountries'}>Filter by Activities</Options>
+        <Options value={'allActivities'}>All contain Activities</Options>
+        {allActivities.length > 0 && allActivities?.map((act) => (
+          <Options key={act.id} value={act.name}>{act.name}</Options>
+        ))}
+      </Selectors>
     </ContainerMenu>
   );
 }
